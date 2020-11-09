046a17d6fdf89c261f687d6adbbfbf080a592945
Lucas Vogelsang <l@lucasvo.com>
2020-11-09 13:03:50 +0000

    Remove commas

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>,
-  </React.StrictMode>,
+    </ApolloProvider>
+  </React.StrictMode>
   document.getElementById('root')
 );
 

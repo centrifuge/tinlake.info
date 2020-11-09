3ff3120f7c0387a30482d54102188e64ca1fac91
Lucas Vogelsang <l@lucasvo.com>
2020-11-09 00:02:54 +0000

    Fix config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 const process = require('process')
 
-export default {
+let config = {
   "eth": process.env['ETH_RPC'],
   "graph": "https://api.thegraph.com/subgraphs/name/jennypollack/tinlake-v3",
 }
 
+
+export default config

48e5d1b56c7042cd38c1b72ed948df0e54f2097e
Lucas Vogelsang <l@lucasvo.com>
2020-11-12 19:00:18 +0000

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 import './App.css'
 import logo from './logo.svg'
 
-//import mainnetPools from '@centrifuge/tinlake-pools-mainnet'
-//import contractsABI from './dapp.sol.json'
-import {gql, useQuery} from '@apollo/client'
-import ethers from 'ethers'
-import { Line } from 'react-chartjs-2';
+import { StoreProvider, useStoreActions} from 'easy-peasy';
+import { store } from './store';
 
+
+import { gql, useQuery } from '@apollo/client'
+//import { toDAINumberFormat, parseDecimal, parseDate } from './format'
+//
+import { AssetValueAreaChart} from './components/assetvaluechart'
+import { PoolList } from './components/poollist'
+
+//import mainnetPools from '@centrifuge/tinlake-pools-mainnet'
 //function loadPools() {
 //  return mainnetPools.filter((pool) => pool.version == 3 && pool.addresses != null);
 //}
@@ -21,82 +27,25 @@ query {
   }
 }`
 
-
-
-let parseDecimal = (str) => {
-  return ethers.BigNumber.from(str).div(ethers.BigNumber.from("1000000000000000000")).toString();
-}
-
-let parseDate = (str) => {
-  let date = new Date(parseInt(str)*1000)
-  return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
-}
-
-
-class AssetValueAreaChart extends React.Component {
-    prepareData () {
-      let labels = []
-      let assetValue = []
-      let reserve = []
-      this.props.data.days.forEach((d, i) => {
-        labels.push(parseDate(d.id))
-        assetValue.push(parseDecimal(d.assetValue))
-        reserve.push(parseDecimal(d.reserve))
-      })
-
-      return {
-        labels: labels,
-        datasets: [
-          {
-            label: "Asset Value",
-            data: assetValue,
-            lineTension: 0,
-            pointRadius:0,
-            backgroundColor: 'rgba(39, 98, 255, 0.9)'
-          },
-          {
-            label: "Reserve",
-            data: reserve,
-            lineTension: 0,
-            pointRadius:0,
-            backgroundColor: 'rgba(39, 98, 255, 0.5)'
-          },
-        ]
-      }
-    }
-    prepareOptions() {
-        return {
-            maintainAspectRatio: false, // Don't maintain w/h ratio
-            scales: {
-              yAxes: [{
-                stacked: true
-              }]
-            }
-          }
-    }
-    render() {
-        return (
-            <div className="assetValueChart">
-                <Line data={this.prepareData()} options={this.prepareOptions()} />
-            </div>
-        )
-    }
-}
-
 function App() {
     const {loading, data} = useQuery(dailyPool)
+    const loadData = useStoreActions((actions) => actions.loadData);
+    useEffect(() => { loadData() }, [])
     if (loading) {
       return (<h1>Loading</h1>)
     }
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1><img src={logo} alt=""/>tinlake.info</h1>
-      </header>
-      <AssetValueAreaChart data={data}/>
-      <a href="https://tinlake.centrifuge.io">tinlake.centrifuge.io</a>
-      </div>
+      <StoreProvider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <h1><img src={logo} alt=""/>tinlake.info</h1>
+        </header>
+           <AssetValueAreaChart data={data}/>
+           <PoolList />
+        <a href="https://tinlake.centrifuge.io">tinlake.centrifuge.io</a>
+        </div>
+      </StoreProvider>
     )
 }
 
-export default App;
+export default App

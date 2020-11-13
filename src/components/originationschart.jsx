fabd15a7dc485cca4ef246f8e97210beb1fba836
Lucas Vogelsang <l@lucasvo.com>
2020-11-13 03:51:58 +0000

    Added originations chart incl. v2

---
+++ src/components/originationschart.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
+import { toDAINumberFormat, parseDecimal, parseDate } from '../format'
+import { useStoreState } from 'easy-peasy';
+
+export const OriginationsChart= () => {
+    const weeklyOriginations = useStoreState((state) => state.weeklyOriginations);
+
+    let prepareData = () => {
+      let labels = []
+      let originations = []
+      let count = []
+      weeklyOriginations.forEach((d) => {
+        labels.push(parseDate(d.date))
+        originations.push(d.amount)
+        count.push(d.count)
+      })
+      return {
+        labels: labels,
+        datasets: [
+          {
+            label: "Originations",
+            data: originations,
+            lineTension: 0,
+            pointRadius:0,
+            backgroundColor: 'rgba(39, 98, 255, 0.9)'
+          },
+        ]
+      }
+    }
+
+    let prepareOptions = () => {
+        return {
+            maintainAspectRatio: false, // Don't maintain w/h ratio
+            scales: {
+              yAxes: [{
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: (value, index, values) => {
+                        return toDAINumberFormat(value)
+                    }
+                },
+                stacked: true
+              }]
+            }
+        }
+    }
+    return (
+      <div className="originationsChart">
+          <Bar data={prepareData()} options={prepareOptions()} />
+      </div>
+    )
+}
+
+
--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { gql } from '@apollo/client'
-import { graphClient, legacyGraphClient } from './graphsource'
+import { graphClient, legacyGraphClient, currentGraphClient } from './graphsource'
 import { thunk, action, createStore } from 'easy-peasy'
+import { BigNumber } from 'bignumber.js'
 import { parseDecimal } from './format'
+import config from './config'
 
 const loaders = []
 
@@ -14,6 +16,7 @@ query {
     totalRepaysAggregatedAmount
   }
 }`
+
 const poolsQuery = gql`
 query {
   pools {
@@ -25,10 +28,12 @@ query {
   }
 }
 `
+
 loaders.push((actions) => {
     return Promise.all([
       legacyGraphClient.query({query: v2PoolsQuery }).then((query) => {
-        return query.data.pools.map((pool) => {
+        let pools = query.data.pools.filter((p) => config.ignorePools.indexOf(p.id) < 0)
+        return pools.map((pool) => {
           let totalDebt = parseDecimal(pool.totalDebt)
 
           return {
@@ -42,7 +47,8 @@ loaders.push((actions) => {
         })
       }),
       graphClient.query({query: poolsQuery}).then((query) => {
-        return query.data.pools.map((pool) => {
+        let pools = query.data.pools.filter((p) => config.ignorePools.indexOf(p.id) < 0)
+        return pools.map((pool) => {
           let assetValue = parseDecimal(pool.assetValue)
           let reserve = parseDecimal(pool.reserve)
           let poolSize = assetValue.plus(reserve)
@@ -78,9 +84,60 @@ loaders.push((actions) => {
     })
 })
 
+
+const loanData = gql`
+query {
+  loans {
+    id
+    pool {
+      id
+    }
+    opened
+    borrowsAggregatedAmount
+  }
+}
+`
+
+const secondsInWeek = 60*60*24*7
+const getWeek = (d) => {
+    return secondsInWeek*Math.floor(d/secondsInWeek)
+}
+
+loaders.push((actions) => {
+  return currentGraphClient.query({query:loanData}).then((query) => {
+    let loans = query.data.loans.filter((l) => config.ignorePools.indexOf(l.pool.id) < 0)
+    loans = loans.map((l) => {
+      return {
+        key: l.id,
+        dateOpened: new Date(parseInt(l.opened)*1000),
+        amount: parseDecimal(l.borrowsAggregatedAmount),
+      }})
+    let start = getWeek(1588707251+secondsInWeek*8)
+    let stop  = getWeek(new Date().getTime()/1000)
+    let empty = {}
+    for (let i = start; i <= stop; i+=secondsInWeek) {
+      empty[i] = {date: i, count:0, amount: new BigNumber('0')}
+    }
+
+    let weeks = loans.reduce((w, l) => {
+      let date = getWeek(l.dateOpened.getTime()/1000)
+      if (date < start) { return w}
+      let week = w[date]
+      week.amount = week.amount.plus(l.amount)
+      week.count += 1
+      w[date] = week
+      return w
+    }, empty)
+    actions.set({key: 'weeklyOriginations', value: Object.values(weeks)})
+    actions.set({key: 'loans', value: loans})
+  })
+})
+
 const store = createStore({
   pools: {},
-  dailyAssetValue:{days:[]},
+  loans: {},
+  weeklyOriginations: [],
+  dailyAssetValue: { days:[]},
   set: action((state, payload) => {
     state[payload.key] = payload.value;
   }),

3e8f42256a65e3cd51a5cdbf0b55c9155a86437e
Lucas Vogelsang <l@lucasvo.com>
2021-02-10 15:06:27 +0000

    Fixing subgraph source

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const process = require("process");
 
 let config = {
   eth: process.env["ETH_RPC"],
-  graphUrl: "https://api.thegraph.com/subgraphs/name/centrifuge/tinlake-v3",
+  graphUrl: "https://api.thegraph.com/subgraphs/name/centrifuge/tinlake",
   ignorePools: ["0x05597dd9b8e1d4fdb44eb69d20bc3a2feef605e8"],
 };
 
